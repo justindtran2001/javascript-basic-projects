4f08fbade277c54ea54f9a39f70d18a8eaecbab8
Groogu <56988254+justindtran2001@users.noreply.github.com>
2023-01-07 07:20:44 +0000

    FINISH project 01

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const clickBtn = document.getElementById("btn");
+const color = document.querySelector('.color');
+
+clickBtn.addEventListener('click', () => {
+    let randomNum = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum];
+    color.style.color = colors[randomNum];
+});
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const clickBtn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+clickBtn.addEventListener("click", () => {
+    let randomHex = getRandomHex();
+
+    document.body.style.backgroundColor = randomHex;
+    color.textContent = randomHex;
+    color.style.color = randomHex;
+});
+
+function getRandomHex() {
+    let res = '#';
+
+    for (let i = 0; i < 6; ++i) {
+        res += hex[Math.floor(Math.random() * hex.length)];
+    }
+
+    return res;
+}
