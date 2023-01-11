649bff547d7ecf69b5a016699d56d5fc1518b8b6
Justin <justindtran2001@gmail.com>
2023-01-11 21:48:14 +0700

    FINISH project 04

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggleBtn = document.querySelector('.nav-toggle');
+const linksList = document.querySelector(".links");
+
+/** Listeners */
+navToggleBtn.addEventListener('click', () => {
+    linksList.classList.toggle('show-links');
+    // if (linksList.classList.contains('show-links'))
+    //     linksList.classList.toggle('show-links', false);
+    // else
+    //     linksList.classList.toggle('show-links', true);
+});
