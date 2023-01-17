9ea65c3a9ac8f564d552fc11af8bf80f31b2f09c
Justin <justindtran2001@gmail.com>
2023-01-17 22:57:44 +0700

    FINISH project 07

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,45 @@
-//using selectors inside the element
-// traversing the dom
+/**
+ * traversing the dom
+ * BOTTOM-UP APPROACH (from the button up)
+ */
+// const questionBtnList = document.querySelectorAll(".question-btn");
+
+// questionBtnList.forEach((btn, i) => {
+//     btn.addEventListener("click", (e) => {
+//         let question = e.currentTarget.parentElement.parentElement;
+
+//         console.log(question);
+
+//         question.classList.toggle("show-text");
+//     });
+// });
+
+/**
+ * using selectors inside the element
+ * TOP-DOWN APPROACH (from the question down)
+ */
+const questionList = document.querySelectorAll(".question");
+// const questionBtnList = document.querySelectorAll(".question-btn");
+
+// questionBtnList.forEach((btn, i) => {
+//     btn.addEventListener("click", () => {
+//         questionList.forEach((question, q_i) => {
+//             if (q_i === i)
+//                 question.classList.toggle("show-text");
+//             else
+//                 question.classList.remove("show-text");
+//         });
+//     });
+// });
+
+questionList.forEach((question, i) => {
+    const btn = question.querySelector(".question-btn");
+
+    btn.addEventListener("click", () => {
+        question.classList.toggle("show-text");
+
+        questionList.forEach((otherQuestion, j) => {
+            if (j !== i) otherQuestion.classList.remove("show-text");
+        });
+    })
+});
